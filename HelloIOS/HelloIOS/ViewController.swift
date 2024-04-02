86a59e002b18ea46be710947084f8dc73f68a46a
audreydk <audreydk@cs.washington.edu>
2024-04-02 13:56:22 -0700

    create project and add 'Go Seahawks'

---
+++ HelloIOS/HelloIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by Audrey Kim on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var UILabel: UILabel!
+    
+}
+
